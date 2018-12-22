b9d7989ec99f4fde30c0a36a188a89c68d6d89ae
Pa Malick Senghore <pama.seng00@gmail.com>
2018-12-22 18:46:07 +0000

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+function opennav(){
+    var navigation = document.querySelector('.nav');
+    if (navigation.style.left == "-500px"){
+        navigation.style.left = "0";
+    }
+    else{
+        navigation.style.left = "-500px";
+    }
+}
